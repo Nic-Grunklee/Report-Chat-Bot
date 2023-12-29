40a1f135165f00d6ffa55060e26ab7a27639a96e
Nic Grunklee <nicgrunklee@gmail.com>
2023-12-28 21:46:13 -0600

    transform csv to txt for easier document reading

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,16 +31,8 @@ def load_data():
         PERSIST_DIR = "./storage"
         if not os.path.exists(PERSIST_DIR):
           
-          SimpleCSVReader = download_loader("SimpleCSVReader", custom_path="local_dir")
-
-          st.write(os.getcwd())
-
-          loader = SimpleCSVReader(encoding="utf-8")
-          csv = loader.load_data(file=Path('./data/occupation-salary-hourly.csv"'))
-
-
           reader = SimpleDirectoryReader(input_dir="./data", recursive=True)
-          docs = reader.load_data() + csv
+          docs = reader.load_data()
 
           service_context = ServiceContext.from_defaults(llm=OpenAI(model="gpt-3.5-turbo", temperature=0.5, 
                                                                     system_prompt="You are an expert on this CareerScope report. Your job is to answer technical questions. Assume that all questions are related to the assessment profile report and occupation hourly and annual rates information. Keep your answers technical and based on facts – do not hallucinate features."))
